ce45d8e0c64690e4f9b1e949f300e0092740344a
AntiD2ta <mtpotro41@gmail.com>
2020-10-30 16:21:06 -0400

    [Strings] - Add Strings module for fixed texts

---
+++ src/Strings.hs
@@ -0,0 +1,15 @@
+module Strings
+(
+ intro,
+ please,
+ options,
+ example
+) where
+
+intro = "Welcome to Hidato solver!!! \n\nPress Ctrl+D anytime to exit\n"
+please = "Please select a option: \n"
+options = ["1-) Write an Hidato to be solved",
+           "2-) Take an Hidato from a file",
+           "3-) Generate a random Hidato"]
+
+example = "0 6 -\n10 0 7\n9 8 0\n1 0 -\n"
